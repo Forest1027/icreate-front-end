8811a5d65b16eae2e46a1ea086822f79e473c6c2
Forest1027 <azureforestryan@gmail.com>
2021-02-09 19:56:20 +0000

    article set up

---
+++ src/components/UI/Search/Search.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {fade, makeStyles} from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade('#660033', 0.1),
+        '&:hover': {
+            backgroundColor: fade('#660033', 0.2),
+        },
+        marginTop: theme.spacing(5),
+        marginBottom: theme.spacing(5),
+        justifyContent: 'center',
+        margin: 'auto',
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'inherit'
+    },
+    inputRoot: {
+        color: 'inherit',
+
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
+
+const Search = () => {
+    const classes = useStyles();
+    return (
+        <Grid item className={classes.search} sm={6} xs={8}>
+            <div className={classes.searchIcon}>
+                <SearchIcon/>
+            </div>
+            <InputBase
+                placeholder="Search…"
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                }}
+                inputProps={{'aria-label': 'search'}}
+            />
+        </Grid>);
+};
+
+export default Search;
