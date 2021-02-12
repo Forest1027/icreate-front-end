52d0747c507731e9d0d799ae03a2d6180c075080
Forest1027 <azureforestryan@gmail.com>
2021-02-12 07:59:13 +0000

    create/cancel button in ArticleDetail

--- src/components/Article/ArticleDetail/ArticleDetail.js
+++ src/components/Article/ArticleDetail/ArticleDetail.js
@@ -4,6 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import Box from "@material-ui/core/Box";
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -21,6 +22,25 @@ const useStyles = makeStyles((theme) => ({
     editor: {
         marginTop: theme.spacing(5),
     },
+    cancelButton: {
+        borderColor: '#660033',
+        color: '#660033',
+        width: 80,
+        height: '100%',
+        textAlign: 'left',
+        marginRight: theme.spacing(1),
+    },
+    editButton: {
+        backgroundColor: '#660033',
+        color: 'white',
+        width: 80,
+        height: '100%',
+        textAlign: 'left',
+        marginRight: theme.spacing(1),
+    },
+    buttonBox: {
+        textAlign: 'left',
+    }
 }));
 const ArticleDetail = () => {
     const classes = useStyles();
@@ -29,6 +49,10 @@ const ArticleDetail = () => {
         <Box justifyContent="center" display="flex" flexWrap='wrap'>
             <Box justifyContent="flex-start" width='80%'>
                 <form className={classes.root} noValidate autoComplete="off">
+                    <Box className={classes.buttonBox}>
+                        <Button className={classes.editButton} variant="contained">Create</Button>
+                        <Button className={classes.cancelButton} variant="outlined">Cancel</Button>
+                    </Box>
                     <Box className={classes.title}>
                         <TextField id="standard-basic" label="Title" fullWidth/>
                     </Box>
